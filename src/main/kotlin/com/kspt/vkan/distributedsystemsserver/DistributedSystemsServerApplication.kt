d38e9c005cd67a093bf78cf7980caa425bf5b6c1
Vitaly Kan <vitalikkan@gmail.com>
2018-12-19 21:52:31 +0300

    Add initial Spring Boot components

---
+++ src/main/kotlin/com/kspt/vkan/distributedsystemsserver/DistributedSystemsServerApplication.kt
@@ -0,0 +1,12 @@
+package com.kspt.vkan.distributedsystemsserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DistributedSystemsServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<DistributedSystemsServerApplication>(*args)
+}
+
---
+++ src/test/kotlin/com/kspt/vkan/distributedsystemsserver/DistributedSystemsServerApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.kspt.vkan.distributedsystemsserver
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class DistributedSystemsServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
+
